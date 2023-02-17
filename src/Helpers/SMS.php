fe90035c6b912e973f0864e21ca7c4b28e8962e0
kemboi22 <kemboielvis22@gmail.xom>
2023-02-17 17:13:50 +0300

    SendSMS created and Other methods

---
+++ src/Helpers/SMS.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Kemboielvis22\SmsgeniusPhpSdk\Helpers;
+
+class SMS extends \Kemboielvis22\SmsgeniusPhpSdk\SMSGenius
+{
+    protected string $smsID;
+    protected string $campaignID;
+
+    protected string $sendSingleSMSURL = "send/sms";
+    protected string $sendBulkSMSURL = "send/sms.bulk";
+
+
+    public function __construct($apikey)
+    {
+        parent::__construct($apikey);
+    }
+
+    public function smsID($smsID): static
+    {
+        $this->smsID = $smsID;
+        return $this;
+    }
+
+    public function campaignID($campaignID): static
+    {
+        $this->campaignID = $campaignID;
+        return $this;
+    }
+
+    public function sendSingleSMS($array)
+    {
+
+
+    }
+
+    public function sendBulkSMS()
+    {
+
+    }
+
+    public function deleteReceivedMessages($smsID = "")
+    {
+        if ($smsID != "")
+        {
+            $this->smsID($smsID);
+        }
+        $deleteReceivedSMSURL = "delete/sms.received?secret=".$this->apikey."&id=".$this->smsID;
+        return $this->getCurl($deleteReceivedSMSURL);
+    }
+
+    public function deleteSMSCampaign($campaignID = "")
+    {
+        if ($campaignID != "")
+        {
+            $this->campaignID($campaignID);
+        }
+        $deleteCampaignURL = "delete/sms.campaign?secret={$this->apikey}&id={$this->campaignID}";
+        return $this->getCurl($deleteCampaignURL);
+    }
+
+    public function deleteSentMessage($smsID= "")
+    {
+        if ($smsID != ""){
+            $this->smsID($smsID);
+        }
+        $deleteSentSMSURL = "delete/sms.sent?secret={$this->apikey}&id={$this->smsID}";
+        return $this->getCurl($deleteSentSMSURL);
+
+    }
+
+    public function getDevices()
+    {
+        $getDevicesURL = "get/devices?secret={$this->apikey}";
+        return $this->getCurl($getDevicesURL);
+
+    }
+
+    public function getPendingMessages()
+    {
+        $getPendingSMSURL = "get/sms.pending?secret={$this->apikey}";
+        return $this->getCurl($getPendingSMSURL);
+
+    }
+
+    public function getReceivedMessages()
+    {
+        $getReceivedSMSURL = "get/sms.received?secret={$this->apikey}";
+        return $this->getCurl($getReceivedSMSURL);
+    }
+
+    public function getSentMessages()
+    {
+        $getSentSMSURL = "get/sms.sent?secret={$this->apikey}";
+        return $this->getCurl($getSentSMSURL);
+        
+    }
+
+
+
+    public function startSMSCampaign($campaignID = "")
+    {
+        if ($campaignID != "")
+        {
+            $this->campaignID($campaignID);
+        }
+        $startSMSCampaignURL = "remote/start.sms?secret={$this->apikey}&campaign={$this->campaignID}";
+        return $this->getCurl($startSMSCampaignURL);
+        
+    }
+
+    public function stopSMSCampaign()
+    {
+        $stopCampaignURL = "remote/stop.sms?secret={$this->apikey}&campaign={$this->campaignID}";
+        return $this->getCurl($stopCampaignURL);
+    }
+
+    public function getSMSCampaigns()
+    {
+        $getSMSCampaignURL = "get/sms.campaigns?secret={$this->apikey}";
+        return $this->getCurl($getSMSCampaignURL);
+
+    }
+
+}
--- src/SMSGenius.php
+++ src/SMSGenius.php
@@ -2,8 +2,41 @@
 
 namespace Kemboielvis22\SmsgeniusPhpSdk;
 
+use Kemboielvis22\SmsgeniusPhpSdk\Helpers\SMS;
+
 class SMSGenius
 {
+    protected string $apikey ;
+    protected string $baseurl = "https://sms.genius.ke/api/";
+    public function __construct($apikey)
+    {
+        $this->apikey = $apikey;
+    }
+
+    public function SMS(): SMS
+    {
+        return new SMS($this->apikey);
+    }
+
+    protected function postCurl($url, $array)
+    {
+        $cURL = curl_init($this->baseurl.$url);
+        curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($cURL, CURLOPT_POSTFIELDS, $array);
+        $response = curl_exec($cURL);
+        curl_close($cURL);
+        return json_decode($response, true);
+    }
+
+    protected function getCurl($url)
+    {
+        $cURL = curl_init();
+        curl_setopt($cURL, CURLOPT_URL, $this->baseurl.$url);
+        curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($cURL);
+        curl_close($cURL);
+        return json_decode($response, true);
+    }
 
 
 }
